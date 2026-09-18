f34969f877442eb07d3aa5467e485ca17f359e51
Neil <neil@logan-5.co.uk>
2026-09-18 19:16:28 +0100

    Style card scrollbar to match neon theme; bump cache version

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service worker for offline use. Bump CACHE_NAME whenever cached files change
 // so old caches are cleared out and users pick up the new version.
-const CACHE_NAME = 'diyas-flashcards-v2';
+const CACHE_NAME = 'diyas-flashcards-v3';
 
 const PRECACHE_ASSETS = [
   './',
