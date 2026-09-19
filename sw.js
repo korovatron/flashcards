a11279002f444517ab9491f604505a5e17d0c7cf
Neil <neil@logan-5.co.uk>
2026-09-19 10:19:19 +0100

    Bumped cache version to v9 in sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service worker for offline use. Bump CACHE_NAME whenever cached files change
 // so old caches are cleared out and users pick up the new version.
-const CACHE_NAME = 'diyas-flashcards-v8';
+const CACHE_NAME = 'diyas-flashcards-v9';
 
 const PRECACHE_ASSETS = [
   './',
